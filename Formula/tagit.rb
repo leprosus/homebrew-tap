680655190bbae5003161f67eec169d7d53edb2e2
leprosus <admin@leprosus.ru>
2026-09-25 19:13:49 +0200

    Add: v0.2.0

--- Formula/tagit.rb
+++ Formula/tagit.rb
@@ -1,8 +1,8 @@
 class Tagit < Formula
   desc "To create and increment semantic Git tags"
   homepage "https://github.com/leprosus/tagit"
-  url "https://github.com/leprosus/tagit/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f861e95e5dda080541f51891461358a36450ea1baff09233e738b5957db1b356"
+  url "https://github.com/leprosus/tagit/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "9462724060efe7cdfea6104c9b56f0de718099b792514fd0535beb42b874af7f"
   license "MIT"
 
   depends_on "go" => :build
